9f5640655813e5be048ee051665dbfbf70e9b1a6
Italo Git <sulamita.italo12@gmail.com>
2024-07-04 15:36:50 -0300

    Reajuste de Padrão e Add de Página

--- src/componentes/Cosmopolitan/cosmopolitan.jsx
+++ src/componentes/Cosmopolitan/cosmopolitan.jsx
@@ -1,19 +1,22 @@
-import styles from './cosmopolitan.module.css'
+import styles from './cosmopolitan.module.css';
+import Link from 'next/link';
 
 export default function Cosmopolitan() {
     return (
-        <div className={styles.drink}>
-            <div className={styles.foto}></div>
-            <div className={styles.descricao}>
-              <h3>
-                Cosmopolitan
-              </h3>
-              <ul>
-                <li>Nível de Dificuldade: Medio</li>
-                <li>Este drink é servido em um taça.</li>
-                <li>Ingredientes: Vodka, Licor de Laranja, Xarope Cramberry e Suco de Limão.</li>
-              </ul>
-            </div>
-        </div>
+          <div className={styles.drink}>
+                <div className={styles.foto}></div>
+                <div className={styles.descricao}>
+                  <Link href="cosmopolitan" className={styles.pag}>
+                    <h3>
+                      Cosmopolitan
+                    </h3>
+                  </Link>
+                  <ul>
+                    <li>Nível de Dificuldade: Medio</li>
+                    <li>Este drink é servido em um taça.</li>
+                    <li>Ingredientes: Vodka, Licor de Laranja, Xarope Cramberry e Suco de Limão.</li>
+                  </ul>
+                </div>
+          </div>
     )
 }
---
+++ src/componentes/header/header.jsx
@@ -0,0 +1,17 @@
+import styles from './header.module.css';
+import Logo from '../Logo/drink.png';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Header() {
+    return(
+        <header className={styles.topo}>
+          <Image src={Logo} className={styles.logo}/>
+          <nav className={styles.links}>
+            <Link href="#">Home</Link>
+            <Link href="#">Catalogo</Link>
+            <Link href="#">Contato</Link>
+          </nav>
+        </header>
+    )
+}
